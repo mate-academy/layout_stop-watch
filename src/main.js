09cda9d7b12cca6a302f603137256ba99bfa6e64
Dmitry Koka <dmitrykoka@gmail.com>
2019-06-01 18:27:19 +0300

    Solution

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+function incrementSeconds() {
+  let seconds = 0;
+  seconds += 1;
+  document.querySelector('.sec').innerText = seconds + ' секунд';
+}
+
+setInterval(incrementSeconds, 1000);
