33c1447c97672ba245b2b5a855792d806192fcf8
Dmitry Koka <dmitrykoka@gmail.com>
2019-06-01 19:47:11 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 'use strict';
+let seconds = 1;
+let min = 0;
 
 function incrementSeconds() {
-  let seconds = 0;
-  seconds += 1;
   document.querySelector('.sec').innerText = seconds + ' секунд';
+  document.querySelector('.min').innerText = min + ' минут';
+  seconds += 1;
+  if (seconds === 60) {
+    min += 1;
+    seconds = 1;
+  }
 }
 
 setInterval(incrementSeconds, 1000);
