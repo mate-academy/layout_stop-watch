5b59ef491ea5468968160803437f530ef1be2edc
gestap <gestap9@gmail.com>
2019-07-25 11:39:31 +0300

    added module backstopConfig

--- config/backstop/backstopConfig.js
+++ config/backstop/backstopConfig.js
@@ -1,54 +1,27 @@
 'use strict';
 
 // https://github.com/garris/BackstopJS#advanced-scenarios
-const basicScenario = {
-  label: 'test', // name of the test
-  url: 'http://localhost:8080/index.html',
-  referenceUrl: '', // put here reference to github-pages with ready project
-  readyEvent: '',
-  readySelector: '',
-  delay: 1000,
-  hideSelectors: ['[data-qa-hidden]'],
-  removeSelectors: ['[data-qa-remove]'],
-  hoverSelector: '',
-  clickSelector: '',
-  postInteractionWait: 0,
-  selectors: '',
-  selectorExpansion: true,
-  expect: 0,
-  misMatchThreshold: 1, // 1% of 100%
-  requireSameDimensions: true,
+
+const backstop = require('@mate-academy/backstop-config');
+
+const { basicScenario } = backstop;
+
+const basic = {
+  ...basicScenario,
+  label: 'Elementary test',
+  referenceUrl: '',
 };
 
-module.exports = {
-  id: 'test',
+const config = {
+  ...backstop,
   onBeforeScript: 'puppet/onBefore.js',
   onReadyScript: 'puppet/onReady.js',
-  viewports: [
+  viewports: [],
+  scenarios: [
     {
-      name: 'tablet_h',
-      width: 1024,
-      height: 768,
+      ...basic,
     },
   ],
-  scenarios: [
-    { ...basicScenario },
-    // define here scenarios for testing
-  ],
-  paths: {
-    bitmaps_reference: 'backstop_data/bitmaps_reference',
-    bitmaps_test: 'backstop_data/bitmaps_test',
-    engine_scripts: 'backstop_data/engine_scripts',
-    html_report: 'backstop_data/html_report',
-    ci_report: 'backstop_data/ci_report',
-  },
-  report: ['browser'],
-  engine: 'puppeteer',
-  engineOptions: {
-    args: ['--no-sandbox'],
-  },
-  asyncCaptureLimit: 5,
-  asyncCompareLimit: 50,
-  debug: false,
-  debugWindow: false,
 };
+
+module.exports = config;
