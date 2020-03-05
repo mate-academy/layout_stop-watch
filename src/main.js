95ccf5c6ba11ea663560f86ad7421d2bf7fe57de
Andreas_Just <andreas.wq@gmail.com>
2020-03-05 04:04:33 +0200

    edits made

--- src/main.js
+++ src/main.js
@@ -1,65 +1,50 @@
 'use strict';
 
-init();
+const MARKS_COUNT = 12;
 
-function init() {
+const startDrawScale = () => {
   const clock = document.querySelector('.clock');
-  const widthClock = window.getComputedStyle(clock, null).width;
+  const clocksWidth = clock.getBoundingClientRect().width;
 
-  for (let i = 0; i < 11; i++) {
-    const div = document.createElement('div');
-    div.classList.add(`scale`);
-    clock.prepend(div);
+  for (let i = 1; i < MARKS_COUNT; i++) {
+    const mark = document.createElement('div');
+    mark.classList.add('scale');
+    clock.prepend(mark);
   }
 
-  const scale = clock.querySelectorAll('.scale');
-  drawScaleClock(scale, widthClock);
-  drawScaleNumClock(scale);
-}
+  const scaleMarks = clock.querySelectorAll('.scale');
+  drawScaleClock(scaleMarks, clocksWidth);
+  drawScaleNumClock(scaleMarks);
+};
 
-function drawScaleClock(scale, widthClock) {
-  const width = parseInt(widthClock, 10);
-  const delta = Math.PI * 2 / scale.length;
+const drawScaleClock = (scale, width) => {
+  const delta = Math.PI * 2 / MARKS_COUNT;
   let angle = 0;
   let angleScale = 60;
 
-  for (let i = 0; i < scale.length; i++) {
+  for (let i = 0; i < MARKS_COUNT; i++) {
     scale[i].style.top
-      = 100 * Math.sin(angle) * (width / 235) + (width / 2.22) + 'px';
+      = 100 * Math.sin(angle) * (width / 235) + (width / 2.21) + 'px';
     scale[i].style.left
-      = 100 * Math.cos(angle) * (width / 235) + (width / 2) + 'px';
+      = 100 * Math.cos(angle) * (width / 235) + (width / 2.02) + 'px';
     scale[i].style.transform
       = `translateX(-50%) rotate(${angleScale += 30}deg)`;
 
     angle += delta;
   }
-}
+};
 
-function drawScaleNumClock(scale) {
-  for (let i = 0; i <= scale.length; i++) {
-    switch (i) {
-      case 0:
-        scale[i].insertAdjacentText('beforeEnd', 3);
-        changeStyleNumClock(scale, i);
-        break;
-      case 3:
-        scale[i].insertAdjacentText('beforeEnd', 6);
-        changeStyleNumClock(scale, i);
-        break;
-      case 6:
-        scale[i].insertAdjacentText('beforeEnd', 9);
-        changeStyleNumClock(scale, i);
-        break;
-      case 9:
-        scale[i].insertAdjacentText('beforeEnd', 12);
-        changeStyleNumClock(scale, i);
-        break;
-    }
+const drawScaleNumClock = scale => {
+  for (let i = 0; i < MARKS_COUNT; i += 3) {
+    scale[i].insertAdjacentText('beforeEnd', i + 3);
+    changeStyleNumClock(scale, i);
   }
-}
+};
 
-function changeStyleNumClock(scale, index) {
+const changeStyleNumClock = (scale, index) => {
   scale[index].classList.remove('scale');
   scale[index].classList.add('scale-number');
   scale[index].style.transform = 'translateX(-50%)';
-}
+};
+
+startDrawScale();
