e555c3a777f7a924733f781f86d28310199ab856
Andrey Grigoriev <nkgrand25@gmail.com>
2021-08-21 23:12:39 +0300

    added clock face change and reset btns actions  with js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const firstClockFace = document.querySelector('.btn__firstSkin');
+const secondClockFace = document.querySelector('.btn__secondSkin');
+const clearClockFace = document.querySelector('.btn__clearSkin');
+const clock = document.querySelector('.clock');
+
+firstClockFace.addEventListener('click', function() {
+  clock.style.background = `url('clock.155a95e0.png')`;
+});
+
+secondClockFace.addEventListener('click', function() {
+  clock.style.background = `url('clockwatch.d7b4ec40.png')`;
+});
+
+clearClockFace.addEventListener('click', function() {
+  clock.style.background = 'none';
+});
