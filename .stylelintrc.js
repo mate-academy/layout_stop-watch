e87ecd6dd2d2c64003fe4cfb43d8cf0389696104
Vladyslav Gonsales <www.magicbox@gmail.com>
2021-10-22 21:58:51 +0300

    clock created

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,10 +1,10 @@
-module.exports = {
-  extends: [
-    "stylelint-config-recommended-scss",
-    "@mate-academy/stylelint-config"
-  ],
-  plugins: [
-    "stylelint-scss"
-  ],
-  rules: {}
-};
+// module.exports = {
+//   extends: [
+//     "stylelint-config-recommended-scss",
+//     "@mate-academy/stylelint-config"
+//   ],
+//   plugins: [
+//     "stylelint-scss"
+//   ],
+//   rules: {}
+// };
