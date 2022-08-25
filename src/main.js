371cc233bd5c86b756c3aa46887f680bf03b2de7
Kamal Tufekcic <kamal.tufekcic@gmail.com>
2022-08-25 08:06:47 +0300

    If it looks stupid but works, it's not stupid

--- src/main.js
+++ src/main.js
@@ -3,11 +3,27 @@
 const sHand = document.querySelector('.clock__hand--s');
 const mHand = document.querySelector('.clock__hand--m');
 const hHand = document.querySelector('.clock__hand--h');
-const click = document.getElementById('secret__div');
-let sCounter = 0;
+const click = document.querySelector('.secret__div');
+const rickRoll = document.querySelector('#secret');
+const pause = document.querySelector('#pause');
+const stop = document.querySelector('#stop');
 let sDegrees = 90;
 let mDegrees = 90;
 let hDegrees = 90;
+let sCounter = 0;
+let interval;
+
+pause.onchange = function() {
+  pauseHandler();
+};
+
+stop.onclick = function() {
+  stopHandler();
+};
+
+rickRoll.onplay = function() {
+  bigSecret();
+};
 
 function timer() {
   sDegrees++;
@@ -26,11 +42,25 @@ function timer() {
   hHand.style.transform = `rotate(${hDegrees}deg)`;
 }
 
-setInterval(timer, 166.67);
+function pauseHandler() {
+  if (pause.checked) {
+    interval = setInterval(timer, 166.66667);
+  } else {
+    clearInterval(interval);
+  }
+}
 
-document.getElementById('secret').onplay = function() {
-  bigSecret();
-};
+function stopHandler() {
+  sCounter = 0;
+  sDegrees = 90;
+  mDegrees = 90;
+  hDegrees = 90;
+  pause.checked = false;
+  clearInterval(interval);
+  sHand.style.transform = `rotate(${sDegrees}deg)`;
+  mHand.style.transform = `rotate(${mDegrees}deg)`;
+  hHand.style.transform = `rotate(${hDegrees}deg)`;
+}
 
 function bigSecret() {
   click.style.display = 'none';
