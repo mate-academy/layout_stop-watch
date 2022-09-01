108826f8a41236a4d8edc95fe20b6e07ecdd1b67
fatihsamur <a.fatihsamur@gmail.com>
2022-09-01 17:49:03 +0300

    BEM refactored

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const btnStart = document.querySelector('.btn__start');
-const btnStop = document.querySelector('.btn__stop');
-const btnReset = document.querySelector('.btn__reset');
+const btnStart = document.querySelector('.control__btn--start');
+const btnStop = document.querySelector('.control__btn--stop');
+const btnReset = document.querySelector('.control__btn--reset');
 
 const clockHands = document.querySelectorAll('.clock__hand');
 const hourHand = document.querySelector('.clock__hand--hour');
@@ -10,19 +10,19 @@ const minHand = document.querySelector('.clock__hand--min');
 const secHand = document.querySelector('.clock__hand--sec');
 
 btnStart.addEventListener('click', () => {
-  hourHand.classList.add('hour-animation--start');
-  minHand.classList.add('min-animation--start');
-  secHand.classList.add('sec-animation--start');
+  hourHand.classList.add('clock__animation-start--hour');
+  minHand.classList.add('clock__animation-start--min');
+  secHand.classList.add('clock__animation-start--sec');
 });
 
 btnStop.addEventListener('click', () => {
   clockHands.forEach(clockHand => {
-    clockHand.classList.toggle('animation--paused');
+    clockHand.classList.toggle('clock__animation-paused');
   });
 });
 
 btnReset.addEventListener('click', () => {
-  hourHand.classList.remove('hour-animation--start', 'animation--paused');
-  minHand.classList.remove('min-animation--start', 'animation--paused');
-  secHand.classList.remove('sec-animation--start', 'animation--paused');
+  hourHand.classList.remove('clock__animation-start--hour', 'animation-paused');
+  minHand.classList.remove('clock__animation-start--min', 'animation-paused');
+  secHand.classList.remove('clock__animation-start--sec', 'animation-paused');
 });
