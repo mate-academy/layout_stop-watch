f75e1b44c67152e93c6875dc430fc98ee4541e9d
Andrii Dudar <andrij.dudar@gmail.com>
2022-12-07 05:55:44 +0200

    Stop watch

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+document.querySelector('#start-clock').addEventListener('click', () => {
+  document.querySelector('.watch__minute-hand').classList.add('start-minute');
+});
+
+document.querySelector('#start-clock').addEventListener('click', () => {
+  document.querySelector('.watch__second-hand').classList.add('start-second');
+});
+
+document.querySelector('#pause').addEventListener('click', () => {
+  document.querySelector('.watch__second-hand').classList.add('pause');
+});
+
+document.querySelector('#pause').addEventListener('click', () => {
+  document.querySelector('.watch__minute-hand').classList.add('pause');
+});
