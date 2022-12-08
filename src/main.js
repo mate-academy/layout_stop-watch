fb572d8721ccdb7f8a02d03f2ba288900b3ccd3e
@nataliia.vyrsta <natali.vyrsta@gmail.com>
2022-12-09 00:05:02 +0200

    add stopwatch

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const btn = document.querySelector('#btn');
+const secondHand = document.querySelector('#s-hand');
+const msHand = document.querySelector('#ms-hand');
+
+btn.addEventListener('click', (event) => {
+  if (event.target.innerText === 'PAUSE') {
+    event.target.innerText = 'START';
+  } else {
+    event.target.innerText = 'PAUSE';
+  }
+
+  secondHand.classList.toggle('play-animation');
+  msHand.classList.toggle('play-animation');
+});
