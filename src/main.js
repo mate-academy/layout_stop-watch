ce26186150322be92d54caba8484b5bc8c5da268
Andrii Dudar <andrij.dudar@gmail.com>
2022-12-10 15:23:11 +0200

    done

--- src/main.js
+++ src/main.js
@@ -1,18 +1,17 @@
 'use strict';
 
-document.querySelector('#start-clock').addEventListener('click', () => {
+const startButton = document.querySelector('#start-clock');
+const pauseButton = document.querySelector('#pause');
+
+startButton.addEventListener('click', () => {
   document.querySelector('.watch__minute-hand').classList.remove('pause');
   document.querySelector('.watch__second-hand').classList.remove('pause');
-  
+
   document.querySelector('.watch__minute-hand').classList.add('start-minute');
   document.querySelector('.watch__second-hand').classList.add('start-second');
 });
 
-
-document.querySelector('#pause').addEventListener('click', () => {
+pauseButton.addEventListener('click', () => {
   document.querySelector('.watch__second-hand').classList.add('pause');
-});
-
-document.querySelector('#pause').addEventListener('click', () => {
   document.querySelector('.watch__minute-hand').classList.add('pause');
 });
