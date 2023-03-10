70f1e37f23eb9ca05c6a0e2377852ee8e5e892fa
Oleh Kolodii <bigknife4@gmail.com>
2023-03-10 15:27:14 +0200

    added stop/reset button

--- src/main.js
+++ src/main.js
@@ -1,1 +1,28 @@
 'use strict';
+
+let counter = 0;
+const button = document.querySelector('.stopwatch__button');
+const second = document.querySelector('.second');
+const minute = document.querySelector('.minute');
+
+button.addEventListener('click', pauseReset);
+
+function pauseReset() {
+  if (counter === 0) {
+    second.style.animationPlayState = 'paused';
+    minute.style.animationPlayState = 'paused';
+    counter++;
+  } else if (counter === 1) {
+    second.classList.remove('stopwatch__second');
+    minute.classList.remove('stopwatch__minute');
+    void second.offsetWidth;
+    void minute.offsetWidth;
+    minute.classList.add('stopwatch__minute');
+    second.classList.add('stopwatch__second');
+
+    second.style.animationPlayState = 'running';
+    minute.style.animationPlayState = 'running';
+
+    counter = 0;
+  }
+}
