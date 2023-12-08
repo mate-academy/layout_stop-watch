1af9a337f75ea39763b46b490d571817c4f7352d
Volodymyr Vynnyk <vinnikvvv@gmail.com>
2023-12-08 13:05:11 +0200

    fix tabs

--- src/main.js
+++ src/main.js
@@ -1,30 +1,19 @@
 'use strict';
 
-const link1 = document.querySelector('.tab__link--1');
+const buttons = document.querySelectorAll('.tab__link');
+const allContent = document.querySelectorAll('.tab__content');
 
-link1.addEventListener('click', (event) => openCity(event, 'London'));
+buttons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    // eslint-disable-next-line no-shadow
+    buttons.forEach((button) => {
+      button.classList.remove('active');
+    });
+    button.classList.add('active');
 
-const link2 = document.querySelector('.tab__link--2');
-
-link2.addEventListener('click', (event) => openCity(event, 'Paris'));
-
-const link3 = document.querySelector('.tab__link--3');
-
-link3.addEventListener('click', (event) => openCity(event, 'Tokyo'));
-
-function openCity(evt, cityName) {
-  const tabLink = document.querySelector('.tab__link');
-  const content = document.querySelector('.tab__content');
-
-  if (content.classList.contains('tab__content')) {
-    content.style.display = 'none';
-  }
-  // content.style.display = 'none';
-
-  if (tabLink.classList.contains('active')) {
-    tabLink.classList.remove('active');
-  }
-
-  document.getElementById(cityName).style.display = 'block';
-  evt.currentTarget.className += ' active';
-};
+    allContent.forEach(content => {
+      content.classList.remove('active');
+    });
+    allContent[index].classList.add('active');
+  });
+});
